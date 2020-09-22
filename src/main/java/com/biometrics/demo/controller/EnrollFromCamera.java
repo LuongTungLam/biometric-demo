1a2dd80f19d591f74f6726a96684213c93860aa6
lamluong <lamluong5433@gmail.com>
2020-09-22 13:02:36 +0700

    camera is null no changeListener

--- src/main/java/com/biometrics/demo/controller/EnrollFromCamera.java
+++ src/main/java/com/biometrics/demo/controller/EnrollFromCamera.java
@@ -11,17 +11,15 @@ import com.neurotec.devices.NDeviceManager;
 import com.neurotec.devices.NDeviceType;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
-import javafx.beans.property.ReadOnlyIntegerProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.SelectionModel;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.FileChooser;
@@ -29,6 +27,8 @@ import lombok.extern.log4j.Log4j2;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Component;
 
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.io.IOException;
 import java.util.EnumSet;
 
@@ -38,11 +38,8 @@ import java.util.EnumSet;
 public class EnrollFromCamera {
     private boolean capturing;
     private FileChooser fc;
-
     @FXML
     private Button cameraChoose;
-    @FXML
-    private Pane cameraView;
     @FXML
     private ComboBox comboBoxCamera;
     @FXML
@@ -51,6 +48,14 @@ public class EnrollFromCamera {
     private final NBiometricClient biometricClient;
     private final FaceViewNode faceViewNode;
     private NSubject subject;
+    private class CameraSelectionListener implements ChangeListener<NCamera> {
+        @Override
+        public void changed(ObservableValue<? extends NCamera> observableValue, NCamera nCamera, NCamera camera) {
+            if (camera != null) {
+                biometricClient.setFaceCaptureDevice(camera);
+            }
+        }
+    }
 
     public EnrollFromCamera(NBiometricClient biometricClient) {
         super();
@@ -61,59 +66,56 @@ public class EnrollFromCamera {
         deviceManager.initialize();
         fc = new FileChooser();
         this.faceViewNode = new FaceViewNode();
-//        updateCamerasList();
+    }
+
+    private void updateCamerasList() {
+        for (NDevice device : deviceManager.getDevices()) {
+            ObservableList<NDevice> options = FXCollections.observableArrayList(device);
+            comboBoxCamera.getItems().addAll(options);
+        }
+//        comboBoxCamera.getSelectionModel().selectedItemProperty().addListener((ChangeListener<NCamera>) (observableValue, oldCamera, camera) -> {
+//            if (camera != null) {
+//                biometricClient.setFaceCaptureDevice(camera);
+//            }
+//        });
+        NCamera camera = biometricClient.getFaceCaptureDevice();
+        if (camera == null) {
+            comboBoxCamera.getSelectionModel().selectFirst();
+        } else if (camera != null) {
+            comboBoxCamera.getSelectionModel().select(camera);
+        }
     }
 
     @FXML
-    public void initialize() {
-        for (NDevice device : deviceManager.getDevices()) {
-//            ObservableList<NDevice> options = FXCollections.observableArrayList(device);
-            comboBoxCamera.getItems().addAll(device);
-        }
-
-        NCamera camera = biometricClient.getFaceCaptureDevice();
-        if (camera == null) {
-            comboBoxCamera.getSelectionModel().select(0);
-            comboBoxCamera.getSelectionModel().getSelectedIndex();
-        } else if (camera != null) {
-            comboBoxCamera.getSelectionModel().getSelectedItem();
-        }
+    public void initialize() throws IOException {
+        comboBoxCamera.valueProperty().addListener(new CameraSelectionListener());
+        updateCamerasList();
         this.paneView.getChildren().add(faceViewNode);
         this.paneView.setAlignment(Pos.CENTER);
-
-        cameraChoose.setOnAction(
-                actionEvent -> {
-                    try {
-                        cameraChose();
-                    }catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                });
     }
 
-    private void cameraChose() throws IOException {
+    @FXML
+    public void onLoad(ActionEvent event){
         Alert a = new Alert(Alert.AlertType.NONE);
-        if (biometricClient.getFaceCaptureDevice() == null){
+        if (biometricClient.getFaceCaptureDevice() == null) {
             EventHandler<ActionEvent> handler = new EventHandler<ActionEvent>() {
                 @Override
                 public void handle(ActionEvent event) {
                     a.setAlertType(Alert.AlertType.ERROR);
-
                     a.setContentText("Face Capture Device Null");
-
                     a.show();
                 }
             };
             return;
+        }else{
+            NFace face = new NFace();
+            EnumSet<NBiometricCaptureOption> options = EnumSet.of(NBiometricCaptureOption.STREAM);
+            face.setCaptureOptions(options);
+            subject = new NSubject();
+            subject.getFaces().add(face);
+            biometricClient.capture(subject);
+            this.faceViewNode.setFace(face);
+            capturing = true;
         }
-        NFace face = new NFace();
-        EnumSet<NBiometricCaptureOption> options = EnumSet.of(NBiometricCaptureOption.STREAM);
-        face.setCaptureOptions(options);
-        subject = new NSubject();
-        subject.getFaces().add(face);
-        biometricClient.capture(subject);
-        this.faceViewNode.setFace(face);
-        capturing = true;
     }
-
 }
